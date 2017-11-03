b616b4593f65571075f1b6ff8b06abf1d35a962b
thewawar <thewawar@gmail.com>
2017-11-04 01:51:44 +0800

    Add a simple bench

---
+++ benches/bench_basic.rs
@@ -0,0 +1,23 @@
+#![feature(test)]
+extern crate test;
+extern crate redis;
+extern crate bucket_limiter;
+
+use test::Bencher;
+use redis::Commands;
+use bucket_limiter::{Limiter, RedisLimiter};
+
+#[bench]
+fn bench_simple(b: &mut Bencher) {
+    let limiter = RedisLimiter::default();
+    let key = "bench_simple";
+    let interval = 600;
+    let capacity = 1000_0000;
+    b.iter(|| {
+        limiter.consume_one(key, interval, capacity);
+    });
+    let _: () = redis::Client::open("redis://127.0.0.1:6379")
+        .unwrap()
+        .del(limiter.get_redis_key(key, interval))
+        .unwrap();
+}
